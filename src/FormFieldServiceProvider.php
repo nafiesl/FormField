6d2882a7aa5b83659395360260d73ad27a5b3b99
Nafies Luthfi <nafiesl@gmail.com>
2017-02-24 11:17:13 +0800

    Refactor service provider

--- src/FormFieldServiceProvider.php
+++ src/FormFieldServiceProvider.php
@@ -13,16 +13,15 @@ class FormFieldServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('formField', function ($app) {
-            return new FormField();
-        });
-
-        $this->app->singleton('html', function ($app) {
-            return new HtmlBuilder($app['url'], $app['view']);
-        });
+        $this->app->alias(FormField::class, 'formField');
 
         $this->app->singleton('form', function ($app) {
-            $form = new FormBuilder($app['html'], $app['url'], $app['view'], $app['session.store']->getToken());
+            $form = new FormBuilder(
+                new HtmlBuilder($app['url'],
+                $app['view']),
+                $app['url'], $app['view'],
+                $app['session.store']->getToken()
+            );
 
             return $form->setSessionStore($app['session.store']);
         });
