9160e1d6f2c55bcb41bb3b90b51e83ed47daf703
Nafies Luthfi <nafiesl@gmail.com>
2018-09-16 19:33:53 +0800

    Add test for checkbox, radio, multiselect validation error

--- tests/Fields/CheckBoxesTest.php
+++ tests/Fields/CheckBoxesTest.php
@@ -99,4 +99,28 @@ class CheckBoxesTest extends TestCase
             $this->formField->checkboxes('checkboxes', [1 => 'Satu', 2 => 'Dua'], ['required' => true])
         );
     }
+
+    /** @test */
+    public function it_shows_checkboxes_with_validation_error()
+    {
+        // Mock error message on "checkboxes" attribute.
+        $errorBag = new \Illuminate\Support\MessageBag();
+        $errorBag->add('checkboxes', 'The checkboxes field is required.');
+
+        $this->formField->errorBag = $errorBag;
+
+        $generatedString = '<div class="form-group has-error">';
+        $generatedString .= '<label for="checkboxes" class="control-label">Checkboxes</label>&nbsp;';
+        $generatedString .= '<ul class="checkbox list-inline">';
+        $generatedString .= '<li><label for="checkboxes_1"><input id="checkboxes_1" name="checkboxes[]" type="checkbox" value="1">Satu&nbsp;</label></li>';
+        $generatedString .= '<li><label for="checkboxes_2"><input id="checkboxes_2" name="checkboxes[]" type="checkbox" value="2">Dua&nbsp;</label></li>';
+        $generatedString .= '</ul>';
+        $generatedString .= '<span class="help-block small">The checkboxes field is required.</span>';
+        $generatedString .= '</div>';
+
+        $this->assertEquals(
+            $generatedString,
+            $this->formField->checkboxes('checkboxes', [1 => 'Satu', 2 => 'Dua'])
+        );
+    }
 }
--- tests/Fields/MultiSelectFieldTest.php
+++ tests/Fields/MultiSelectFieldTest.php
@@ -61,4 +61,30 @@ class MultiSelectFieldTest extends TestCase
             $this->formField->multiSelect('key', [1 => 'Satu', 2 => 'Dua', 3 => 'Tiga'], ['value' => collect([2, 3])])
         );
     }
+
+    /** @test */
+    public function it_shows_multi_select_field_with_validation_error()
+    {
+        // Mock error message on "key" attribute.
+        $errorBag = new \Illuminate\Support\MessageBag();
+        $errorBag->add('key', 'The key field is required.');
+
+        $this->formField->errorBag = $errorBag;
+
+        $generatedString = '<div class="form-group has-error">';
+        $generatedString .= '<label for="key" class="control-label">Key</label>&nbsp;';
+        $generatedString .= '<select class="form-control" multiple name="key[]" id="key">';
+        $generatedString .= '<option value="" selected="selected">-- Select Key --</option>';
+        $generatedString .= '<option value="1">Satu</option>';
+        $generatedString .= '<option value="2">Dua</option>';
+        $generatedString .= '<option value="3">Tiga</option>';
+        $generatedString .= '</select>';
+        $generatedString .= '<span class="help-block small">The key field is required.</span>';
+        $generatedString .= '</div>';
+
+        $this->assertEquals(
+            $generatedString,
+            $this->formField->multiSelect('key', [1 => 'Satu', 2 => 'Dua', 3 => 'Tiga'])
+        );
+    }
 }
